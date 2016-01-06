1804fb013e29698b5ae2ee333d73d8306e24cc65
Blein72 <dolgopolovnik@yandex.ru>
2016-01-06 22:52:35 +0300

    Testing with Gson and adapter! But its not working(

---
+++ Forchanclient/app/src/main/java/com/example/forchanclient/MyListViewAdapter.java
@@ -0,0 +1,51 @@
+package com.example.forchanclient;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Я on 04.01.2016.
+ */
+public class MyListViewAdapter extends ArrayAdapter<Threads> {
+    private  Context context;
+    private  ArrayList<Post> values;
+
+    //public MyListViewAdapter(Context context, ArrayList<Threads> objects) {
+    public MyListViewAdapter(Context context, Threads[] objects) {
+        super(context,R.layout.thread_item, objects);
+        this.context=context;
+        values=new ArrayList<Post>();
+
+        //values=objects.get(0).posts;
+
+        values=objects[0].posts;
+        //values.addAll(objects.get(1).posts);
+       // this.values=objects.get(1).posts;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(R.layout.thread_item, parent, false);
+        TextView name=(TextView)rowView.findViewById(R.id.name);
+        TextView date=(TextView)rowView.findViewById(R.id.date);
+        TextView number=(TextView)rowView.findViewById(R.id.number);
+        TextView comment=(TextView)rowView.findViewById(R.id.comment);
+        name.setText(values.get(position).name);
+        date.setText(values.get(position).now);
+        number.setText(values.get(position).no);
+        comment.setText(values.get(position).com);
+
+        return rowView;
+
+
+
+    }
+}
---
+++ Forchanclient/app/src/main/java/com/example/forchanclient/Post.java
@@ -0,0 +1,17 @@
+package com.example.forchanclient;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Я on 04.01.2016.
+ */
+public class Post {
+    //String no;
+    //@SerializedName("no")
+    public String no;
+    public  String name;
+    //@SerializedName("now")
+    public String now;
+   // @SerializedName("com")
+   public  String com;
+}
